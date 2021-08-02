1c43acbd42950b515493864fccb42e734a9414e5
ShashwatNamdev <77872270+ShashwatNamdev@users.noreply.github.com>
2021-08-02 15:11:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,65 +1,33 @@
-var player;
-var backGround;
-var wall1,wall2,wall3,wall4;
+var database;
+var gameState = 0;
+var playerCount = 0;
+var player,form,game;
+var bgImg;
+var allPlayers;
+var player1,player2,player3,player4;
+var players;
 
 function preload(){
-  bgImg = loadImage("Images/backgroundImg.jpg");
+  bgImg = loadImage("images/backgroundImg_game.jpg");
 }
 
-function setup() {
-  createCanvas(windowWidth/2,windowHeight/2);
-  
-  backGround = createSprite(windowWidth/2,windowHeight/2);
-  backGround.addImage(bgImg);
-  backGround.scale = 3;
-
-  wall1 = createSprite(1780,500,40,2800);
-  wall1.visible = false;
-  wall2 = createSprite(650,1665,2800,40);
-  wall2.visible = false;
-  wall3 = createSprite(0,500,40,2800);
-  wall3.visible = false;
-  wall4 = createSprite(650,-728,2800,40);
-  wall4.visible = false;
-  
-  player = createSprite(windowWidth/2,windowHeight/2,20,20);
-  player.shapeColor = "white";
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
-
-function draw() {
-  background(0);
-  camera.x = player.x;
-  camera.y = player.y;
-
-  console.log(player.x);
-  console.log(player.y);
-
-  if(frameCount%10===0){
-    var zombie = createSprite(400,400,20,20);
-    zombie.shapeColor = "red";
-    zombie.x = random(-400,1580);
-    zombie.y = random(-635,1622);
+function draw(){
+  background("green");
+  if(playerCount===4){
+    game.update(1);
   }
-  player.collide(wall1);
-  player.collide(wall2);
-  player.collide(wall3);
-  player.collide(wall4);
-  
-  keyPressed();
-  drawSprites();
-}
-
-function keyPressed(){
-  if(keyDown("UP_ARROW")){
-    player.y = player.y-9;
+  if(gameState===1){
+    clear();
+    game.play();
   }
-  if(keyDown("DOWN_ARROW")){
-    player.y = player.y+9;
-  }
-  if(keyDown("RIGHT_ARROW")){
-    player.x = player.x+9;
-  }
-  if(keyDown("LEFT_ARROW")){
-    player.x = player.x-9;
+  if(gameState===2){
+    game.end();
   }
 }
